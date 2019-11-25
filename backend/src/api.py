917ac93e7ab19c6361c33a4e96d96baf169a879b
Nelson Mwirumubi <mwinel@live.com>
2019-11-25 09:08:18 +0300

    Add drop/create tables command

--- backend/src/api.py
+++ backend/src/api.py
@@ -11,12 +11,7 @@ app = Flask(__name__)
 setup_db(app)
 CORS(app)
 
-'''
-@TODO uncomment the following line to initialize the datbase
-!! NOTE THIS WILL DROP ALL RECORDS AND START YOUR DB FROM SCRATCH
-!! NOTE THIS MUST BE UNCOMMENTED ON FIRST RUN
-'''
-# db_drop_and_create_all()
+db_drop_and_create_all()
 
 ## ROUTES
 '''
